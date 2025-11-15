75477bc4e7500b71f8bdb4ef6cc592533428824c
matu10583 <oka0121masa@gmail.com>
2025-11-15 19:16:49 +0900

    lib.rs: glob 再エクスポートを明示的な再エクスポートに修正
    警告を解消するため plugin::* と components::* を明示的なシンボル指定に変更

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,6 @@ pub mod platform;
 pub use platform::WinitBackend;
 pub mod events;
 pub mod plugin;
-pub use plugin::*;
+pub use plugin::InputPlugin;
 pub mod components;
-pub use components::*;
+pub use components::{Camera2D, Sprite, Transform2D};
